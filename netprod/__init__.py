4278564aab53b6fc9f8b9eb4dda9184b3b3b1cd6
Roberts <GRoberta@ims.gov.uk>
2017-04-25 13:20:13 +0100

    adding the netprod module for functions

---
+++ netprod/__init__.py
@@ -0,0 +1,7 @@
+print('calling the __init__')
+import pandas as pd
+print('imported pandas')
+import netprod.funcs
+netprod.funcs.func_a()
+
+# now read in the reference data to an object (or call a function to do that)
---
+++ netprod/funcs.py
@@ -0,0 +1,8 @@
+def func_a():
+    '''here is function A'''
+    print("function A executing")
+    return 1
+
+def paid_prod():
+    return 1
+
