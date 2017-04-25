e652c9cc293cbbc68aabf92eb161c1504c56de8d
Roberts <GRoberta@ims.gov.uk>
2017-04-25 17:03:01 +0100

    sorting init

--- netprod/__init__.py
+++ netprod/__init__.py
@@ -1,7 +1,14 @@
 print('calling the __init__')
 import pandas as pd
+import os
 print('imported pandas')
 import netprod.funcs
-netprod.funcs.func_a()
 
-# now read in the reference data to an object (or call a function to do that)
+# Read in the reference data to an object 
+
+# First need to get the right path
+dirpath = netprod.__file__.split("__init__")[0]
+
+#
+prod_model = pd.read_csv(dirpath + "data/paid_prod_model.csv", header=None, index_col=0, names=["value"])
+
--- netprod/funcs.py
+++ netprod/funcs.py
@@ -1,8 +1,12 @@
+import netprod
+
 def func_a():
     '''here is function A'''
     print("function A executing")
     return 1
 
 def paid_prod():
+    '''the docstring here'''
+    print(netprod.prod_model.iloc[0])
     return 1
 
